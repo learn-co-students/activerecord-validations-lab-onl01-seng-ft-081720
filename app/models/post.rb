eae2939a03bd3a4e0aa3e2c8d51a980f8ce19ce9
ellenupark <ellenupark@gmail.com>
2020-10-19 17:33:46 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :must_be_clickbait
+
+    def must_be_clickbait
+        if title
+            if !title.match(/Won't Believe/) && !title.match(/Secret/) && !title.match(/Top \d/) && !title.match(/Guess/)
+                errors.add(:title, "title must be sufficiently clickbait-y")
+            end
+        end
+    end
 end
