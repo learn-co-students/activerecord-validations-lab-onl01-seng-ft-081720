e77522865aa502f0c8bbe30fd36d3d08c757746b
ken <kennethmanuel250@gmail.com>
2020-10-19 22:14:40 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
-end
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w( Fiction Non-Fiction ) } 
+    validate :sufficiently_clickbaity
+
+    def sufficiently_clickbaity
+        if title
+            if !title.match(/Won't Believe/) && !title.match(/Secret/) && !title.match(/Top \d/) && !title.match(/Guess/)
+                errors.add(:title, "The title is not click-baity enough")
+            end 
+        end
+    end 
+end 
