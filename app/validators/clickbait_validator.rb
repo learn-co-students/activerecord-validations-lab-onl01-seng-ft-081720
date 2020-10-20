a8bfb6cea39196cf77eb4df43e572e3884a73a57
mollymoog <mmkover@gmail.com>
2020-10-20 13:34:28 -0400

    Done.

--- app/validators/clickbait_validator.rb
+++
@@ -1,9 +0,0 @@
-class ClickbaitValidator < ActiveModel::Validator
-
-    def validate (record)
-        if !record.includes?( "Won't Believe" || "Secret" || "Top" || "Guess" )
-            
-            record.errors [:title] << "not catchy enough" 
-        end
-    end
-end
