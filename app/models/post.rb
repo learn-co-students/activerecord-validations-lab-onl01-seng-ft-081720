6962a83cbde76b8bd5b5e7563754033532853bca
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-10-30 05:16:05 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    
+    validates :title, presence: true 
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-fiction)}
+    validate :post_title
+
+    CLICKBAIT_ARGS = [
+        "Won't Believe",
+        "Secret",
+        "Top[number]",
+        "Guess"
+    ]
+
+    def post_title 
+        if title.present? && CLICKBAIT_ARGS.none? {|p| title.match(p)}
+            errors.add(:title, "title not clickbait-y enough")
+        end
+    end
 end
